b5c8585947f0da7f17a51fbf3b5f158a78538535
NehaYadavalli <68659585+NehaYadavalli@users.noreply.github.com>
2020-08-06 11:03:39 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,100 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+var red1, red2, red3;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	red1=createSprite(400,650, 200,20);
+	red1.shapeColor=color(235,64,52);
+
+	ground=createSprite(width/2, height-35, width,10);
+	ground.shapeColor=color(255,255,255)
+
+	red1=createSprite(400,650, 200,20);
+	red1.shapeColor=color(235,64,52);
+
+	red2=createSprite(300,590, 20,100);
+	red2.shapeColor=color(235,64,52);
+
+	red3=createSprite(500,590, 20,100);
+	red3.shapeColor=color(235,64,52);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	
+	 
+	 red1 = Bodies.rectangle(400, 650, 200, 20 , {isStatic:true} );
+	 World.add(world, red1);
+	 
+	 red2 = Bodies.rectangle(300, 590, 20, 100 , {isStatic:true} );
+	 World.add(world, red2);
+	 
+	 red3 = Bodies.rectangle(500, 590, 20, 100 , {isStatic:true} );
+	 World.add(world, red3);
+	 
+	 ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	 World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+
+
+
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  keyPressed(); 
+  drawSprites();
+
+
+  
+	
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on
+	Matter.Body.setStatic(packageBody, false);
+  }
+}
+
+
+
+
+
